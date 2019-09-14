767cd37ff2a23c455fb55e5767be14f0f36980b0
Thakkar Parth <thakkarparth7598@outlook.com>
2019-09-14 17:14:02 +0530

    Add project files.

---
+++ AjaxLiveSearch/EmployeeSearch.aspx
@@ -0,0 +1,65 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="EmployeeSearch.aspx.cs" Inherits="AjaxLiveSearch.EmployeeSearch" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <table>
+            <tr>
+                <td>
+                    Name
+                </td>
+                <td>
+                    <input type="text" id="txtname" onkeyup="search();" />
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <asp:DropDownList ID="ddlname" runat="server"
+                        AutoPostBack="false" 
+                        DataSourceID="SqlDataSource1" 
+                        DataTextField="Ename" DataValueField="Ename" 
+                        onchange = "ser()">
+                                              </asp:DropDownList>
+                    <asp:SqlDataSource ID="SqlDataSource1" runat="server" ConnectionString="<%$ ConnectionStrings:AjaxEmpConnectionString %>" SelectCommand="SELECT [Ename] FROM [Employee]"></asp:SqlDataSource>
+                </td>
+            </tr> 
+            
+            
+            
+        </table>
+        <br />
+        <div id="d1">
+
+        </div>
+    </form>
+    <script type="text/javascript">
+        
+        function search() {
+            
+                var a = new XMLHttpRequest();
+            a.open("GET", "insert.aspx?opr=search&nm=" + document.getElementById("txtname").value, false);
+           
+                a.send(null);
+
+                document.getElementById("d1").innerHTML = a.responseText;
+            
+        }
+        function ser() {
+
+            var a = new XMLHttpRequest();
+            a.open("GET", "insert.aspx?opr=search&nm=" + document.getElementById("ddlname").value, false);
+
+            a.send(null);
+
+            document.getElementById("d1").innerHTML = a.responseText;
+
+        }
+    </script>
+    
+</body>
+</html>
---
+++ AjaxLiveSearch/insert.aspx.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace AjaxLiveSearch
+{
+    public partial class insert : System.Web.UI.Page
+    {
+        SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["connstr"].ConnectionString);
+        string opr, Ename;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            opr = Request.QueryString["opr"].ToString();
+            if(opr == "search")
+            {
+                Ename = Request.QueryString["nm"].ToString();
+                con.Open();
+                SqlCommand cmd = new SqlCommand("select * from Employee where Ename like('" + Ename.ToString() + "%')",con);
+                cmd.ExecuteNonQuery();
+                DataTable dt = new DataTable();
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                da.Fill(dt);
+                Response.Write("<table style='boader= bold 1'>");
+                Response.Write("<tr>");
+                Response.Write("<td>"); Response.Write("Name"); Response.Write("</td>");
+                Response.Write("<td>"); Response.Write("Email"); Response.Write("</td>");
+                Response.Write("<td>"); Response.Write("Number"); Response.Write("</td>");
+
+                Response.Write("</tr>");
+
+                foreach(DataRow dr in  dt.Rows)
+                {
+                    Response.Write("<tr>");
+                    Response.Write("<td>"); Response.Write(dr["Ename"].ToString()); Response.Write("</td>");
+                    Response.Write("<td>"); Response.Write(dr["Eemail"].ToString()); Response.Write("</td>");
+                    Response.Write("<td>"); Response.Write(dr["Emnumber"].ToString()); Response.Write("</td>");
+                   
+                    Response.Write("</tr>");
+                }
+                Response.Write("</table>");
+                con.Close();
+            }
+            if (opr == "ser")
+            {
+                Ename = Request.QueryString["nm"].ToString();
+                con.Open();
+                SqlCommand cmd = new SqlCommand("select * from Employee where Ename like('" + Ename.ToString() + "%')", con);
+                cmd.ExecuteNonQuery();
+                DataTable dt = new DataTable();
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                da.Fill(dt);
+                Response.Write("<table style='boader= bold 1'>");
+                Response.Write("<tr>");
+                Response.Write("<td>"); Response.Write("Name"); Response.Write("</td>");
+                Response.Write("<td>"); Response.Write("Email"); Response.Write("</td>");
+                Response.Write("<td>"); Response.Write("Number"); Response.Write("</td>");
+
+                Response.Write("</tr>");
+
+                foreach (DataRow dr in dt.Rows)
+                {
+                    Response.Write("<tr>");
+                    Response.Write("<td>"); Response.Write(dr["Ename"].ToString()); Response.Write("</td>");
+                    Response.Write("<td>"); Response.Write(dr["Eemail"].ToString()); Response.Write("</td>");
+                    Response.Write("<td>"); Response.Write(dr["Emnumber"].ToString()); Response.Write("</td>");
+
+                    Response.Write("</tr>");
+                }
+                Response.Write("</table>");
+                con.Close();
+            }
+
+        }
+    }
+}
